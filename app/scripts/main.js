e63a9916e7b349a760bea33cb01a55a3790ba991
anton <anton.melnichuk@wdg.com.ua>
2014-02-06 19:37:04 +0200

    add all pages, not all fixed

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+	jQuery('.slider').bjqs({
+		animtype      : 'fade',
+		width : 1100,
+		height : 400,
+		showcontrols    : false,
+		centermarkers   : false,
+		responsive      : true,
+		randomstart     : false,
+	});
+});
